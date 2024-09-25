ed3540efe898990dc951167391067ed36b40fe06
Christian Medallada <cbmedallada@student.apc.edu.ph>
2024-09-25 16:16:44 +0800

    Ph 10 e 01 s 02 borrower views request list (#27)
    * Apply the view request list and the filtering it
    
    * Apply the filters and the viewing of Requets List
    
    * Added the search for request and endorser can be search
    
    * View Specific Transaction for the request list
    
    * Fix the Deployment in Vercel and change the request use case
    
    * Fix the Deployment issue
    
    * Change it and revert it to normal

---
+++ src/lib/interfaces/get-specific-transaction-interface.ts
@@ -0,0 +1,43 @@
+// Interface for item details
+ export interface IItem {
+    id: string;
+    model_name: string;
+    quantity: number;
+    start_date: string;
+    due_date: string;
+    borrowed_item_status: string;
+  }
+  
+  // Interface for transaction data
+  export interface ITransacData {
+    id: string;
+    custom_transac_id: string;
+    department_acronym: string;
+    transac_status: string;
+    purpose: string;
+    user_defined_purpose: string | null;
+    penalty: string | null;
+    remarks_by_endorser: string | null;
+    remarks_by_approver: string | null;
+    created_at: string;
+    updated_at: string;
+  }
+  
+  // Interface for the main data object containing transaction and item details
+  interface IGetSpecificTransactionApiResponseDataValue {
+    transac_data: ITransacData;
+    items: IItem;
+  }
+  
+  // Example response type
+  export interface IGetSpecificTransactionApiResponse {
+    status: boolean;
+    data?: IGetSpecificTransactionApiResponseDataValue;
+    error?: any;
+    method: string;
+  }
+
+  export interface ISpecificTransaction {
+    transacId: string;
+  }
+  
