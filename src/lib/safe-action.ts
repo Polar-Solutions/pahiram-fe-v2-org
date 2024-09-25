4c92deacb02185a77da8537fd92fd94ccc9c0ce2
TianMeds <cbmedallada@student.apc.edu.ph>
2024-09-25 13:29:02 +0800

    Change it and revert it to normal

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,7 +1,7 @@
 import { createSafeActionClient } from "next-safe-action";
 
 export const actionClient = createSafeActionClient({
-  handleServerError(e: Error) {
+  handleReturnedServerError(e) {
     return "Oh no, something went wrong!";
   },
 });
