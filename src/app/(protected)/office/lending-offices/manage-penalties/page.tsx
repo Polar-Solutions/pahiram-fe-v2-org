18bd44e0a674f73b1655e437e1a1c6f26976512f
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2024-10-18 12:19:31 +0800

    Resolved deployment issue by commenting out the manage penalties container

--- src/app/(protected)/office/lending-offices/manage-penalties/page.tsx
+++ src/app/(protected)/office/lending-offices/manage-penalties/page.tsx
@@ -1,12 +1,11 @@
 import {ContentLayout} from "@/components/panel/containers/content-layout";
 import Content from "@/components/common/content";
-import ManagePenalties from "@/components/penalty/container/manage-penalties-container";
 
 export default function Page() {
     return (
         <ContentLayout title="Manage Penalty">
             <Content>
-                <ManagePenalties/>
+                {/*<ManagePenalties/>*/}
             </Content>
         </ContentLayout>
     )
